3632f55351b3cfd2f62d1ea615ab832d41d5cf0b
notarious <notarious2@gmail.com>
2023-09-17 22:53:42 +0500

    misc: improve User schema

--- src/api/registration/schemas.py
+++ src/api/registration/schemas.py
@@ -1,12 +1,12 @@
-from pydantic import BaseModel, EmailStr, Extra
+from pydantic import BaseModel, EmailStr, Extra, Field
 
 
 class UserRegisterSchema(BaseModel):
     email: EmailStr
-    username: str
-    password: str
-    first_name: str
-    last_name: str
+    username: str = Field(max_length=150)
+    password: str = Field(min_length=6, max_length=128)
+    first_name: str = Field(max_length=150)
+    last_name: str = Field(max_length=150)
 
     class Config:
         extra = Extra.forbid
