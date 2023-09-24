0144fe923256258cc4d57a323aeb5517301031ff
notarious <notarious2@gmail.com>
2023-09-24 13:31:04 +0500

    feature: Add refresh token endpoint

--- src/api/authentication/router.py
+++ src/api/authentication/router.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter, Depends, HTTPException, Response, status
+from datetime import timedelta
+from typing import Annotated
+
+import jwt
+from fastapi import APIRouter, Cookie, Depends, HTTPException, Response, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api.authentication.services import authenticate_user
+from src.api.authentication.services import authenticate_user, get_user_by_login_identifier
 from src.api.authentication.utils import create_access_token, create_refresh_token
+from src.config import settings
 from src.database import get_async_session
 from src.models import User
 
@@ -36,3 +41,54 @@ async def login(
         "refresh_token": create_refresh_token(login_identifier),
         "username": user.username,
     }
+
+
+@auth_router.post("/refresh/", summary="Create new access token for user")
+async def get_new_access_token_from_refresh_token(
+    refresh_token: Annotated[str, Cookie()],
+    response: Response,
+    db_session: AsyncSession = Depends(get_async_session),
+):
+    try:
+        payload = jwt.decode(refresh_token, settings.JWT_REFRESH_SECRET_KEY, algorithms=[settings.ENCRYPTION_ALGORITHM])
+        login_identifier: str = payload.get("sub")
+        if not login_identifier:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token expired")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Access Token")
+    except jwt.PyJWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    user: User | None = await get_user_by_login_identifier(db_session, login_identifier=login_identifier)
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User is not active",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    new_access_token = create_access_token(
+        login_identifier, expires_delta=timedelta(minutes=settings.NEW_ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+
+    response.set_cookie(key="access_token", value=new_access_token, httponly=True, samesite="none", secure=True)
+
+    return "Access token has been successfully refreshed"
