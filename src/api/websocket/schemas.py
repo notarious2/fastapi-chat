17492cd1c13683da5d486013edd153e4102f3aa9
notarious <notarious2@gmail.com>
2023-12-16 22:43:31 +0500

    Handle New Chat addition while user is online

--- src/api/websocket/schemas.py
+++ src/api/websocket/schemas.py
@@ -31,3 +31,19 @@ class UserTypingSchema(BaseModel):
     type: str
     chat_guid: UUID4
     user_guid: UUID4
+
+
+class NewChatCreated(BaseModel):
+    type: str = "new_chat_created"
+    chat_id: int  # need to pass for guid/id mapping [chats variable]
+    chat_guid: UUID4
+    created_at: datetime
+    updated_at: datetime
+    friend: dict
+    has_new_messages: bool
+    new_messages_count: int
+
+
+class AddUserToChatSchema(BaseModel):
+    chat_guid: str  # used for websocket communication
+    chat_id: int
